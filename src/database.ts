c6d1cabbcb509dcb4202db3bd1292d3a5e5edf3d
lina <36717206+lina-bh@users.noreply.github.com>
2022-04-15 23:47:37 +0100

    low hanging fruit for database code

--- src/database.ts
+++ src/database.ts
@@ -1,46 +1,40 @@
 import * as sqlite from "sqlite";
 import * as discord from "discord.js";
-import BetterSqlite3Database from "better-sqlite3";
 import options from "./options";
 import * as sqlite3 from "sqlite3";
 
 class SqliteDataSource {
     private db?: sqlite.Database;
-    private dbSync?: BetterSqlite3Database.Database;
 
-    private async open(database: string) {
-        this.dbSync = new BetterSqlite3Database(database);
+    async init(database = options.database) {
+        if (this.db) throw new Error("db.init() called twice");
+
         this.db = await sqlite.open({
             filename: database,
             driver: sqlite3.Database,
         });
         console.log(database + " opened");
-    }
 
-    async init(database = options.database) {
-        if (this.db || this.dbSync) {
-            throw new Error("IDataSource.init() was already called");
-        }
-        await this.open(database);
+        await this.db.exec(
+            `CREATE TABLE 
+            IF NOT EXISTS 
+            users (
+                id TEXT NOT NULL, 
+                server TEXT NOT NULL, 
+                balance INTEGER NOT NULL DEFAULT 0, 
+                lastSignon INTEGER NOT NULL DEFAULT 0, 
+                lastStretch INTEGER NOT NULL DEFAULT 0, 
+                PRIMARY KEY (id, server)
+            )`
+        );
+        await this.db.exec(
+            "CREATE TABLE IF NOT EXISTS bans (" +
+                "id TEXT PRIMARY KEY UNIQUE NOT NULL, " +
+                "banned INTEGER NOT NULL DEFAULT 1" +
+                ")"
+        );
 
-        return Promise.all([
-            this.db.exec(
-                "CREATE TABLE IF NOT EXISTS users (" +
-                    "id TEXT NOT NULL, " +
-                    "server TEXT NOT NULL, " +
-                    "balance INTEGER NOT NULL DEFAULT 0, " +
-                    "lastSignon INTEGER NOT NULL DEFAULT 0, " +
-                    "lastStretch INTEGER NOT NULL, " +
-                    "PRIMARY KEY (id, server) " +
-                    ")"
-            ),
-            this.db.exec(
-                "CREATE TABLE IF NOT EXISTS bans (" +
-                    "id TEXT PRIMARY KEY UNIQUE NOT NULL, " +
-                    "banned INTEGER NOT NULL DEFAULT 1" +
-                    ")"
-            ),
-        ]).then(() => console.log("tables created"));
+        console.log("tables created");
     }
 
     async exists(user: discord.User, guild?: discord.Guild): Promise<boolean> {
@@ -64,11 +58,12 @@ class SqliteDataSource {
     }
 
     async update(user: discord.User, guild: discord.Guild) {
-        return this.db!.run(
-            "INSERT OR IGNORE " + "INTO users (id, server) " + "VALUES (?, ?)",
+        await this.db!.run(
+            "INSERT OR IGNORE INTO users (id, server) VALUES (?, ?)",
             user.id,
             guild.id
-        ).then(() => console.log(`${user.username} record updated`));
+        );
+        // console.log(`${user.username} record updated`);
     }
 
     async banned(user: discord.User) {
@@ -83,19 +78,19 @@ class SqliteDataSource {
     }
 
     async ban(user: discord.User) {
-        return this.db!.run(
+        await this.db!.run(
             "INSERT OR REPLACE " + "INTO bans (id) " + "VALUES (?)",
             user.id
         );
     }
 
     async unban(user: discord.User) {
-        return this.db!.run("DELETE FROM bans " + "WHERE id = ?", user.id);
+        await this.db!.run("DELETE FROM bans " + "WHERE id = ?", user.id);
     }
 
     async balance(user: discord.User, server: discord.Guild) {
         const row = await this.db!.get(
-            "SELECT balance " + "FROM users " + "WHERE id = ? AND server = ?",
+            "SELECT balance FROM users WHERE id = ? AND server = ?",
             user.id,
             server.id
         );
@@ -110,16 +105,13 @@ class SqliteDataSource {
         server: discord.Guild,
         amount: number
     ) {
-        return this.db!.run(
-            "UPDATE users " +
-                "SET balance = balance + ? " +
-                "WHERE id = ? AND server = ?",
+        await this.db!.run(
+            "UPDATE users SET balance = balance + ? WHERE id = ? AND server = ?",
             amount,
             user.id,
             server.id
-        ).then(() => {
-            console.log(`${user.username}'s balance incremented by ${amount}`);
-        });
+        );
+        console.log(`${user.username}'s balance incremented by ${amount}`);
     }
 
     async moveMoney(
@@ -128,30 +120,31 @@ class SqliteDataSource {
         server: discord.Guild,
         amount: number
     ) {
-        this.dbSync!.transaction(() => {
-            this.dbSync!.prepare(
-                "UPDATE users " +
-                    "SET balance = balance - :amount " +
-                    "WHERE id = :source AND server = :server" +
-                    ";" +
-                    "UPDATE users " +
-                    "SET balance = balance + :amount " +
-                    "WHERE id = :target AND server = :server"
-            ).run({
-                amount,
-                server: server.id,
-                source: source.id,
-                target: target.id,
-            });
-        });
-        console.log(
-            `${amount} moved from ${source.username} to ${target.username}`
-        );
+        throw new Error("unimplemented");
+        // this.dbSync!.transaction(() => {
+        //     this.dbSync!.prepare(
+        //         "UPDATE users " +
+        //             "SET balance = balance - :amount " +
+        //             "WHERE id = :source AND server = :server" +
+        //             ";" +
+        //             "UPDATE users " +
+        //             "SET balance = balance + :amount " +
+        //             "WHERE id = :target AND server = :server"
+        //     ).run({
+        //         amount,
+        //         server: server.id,
+        //         source: source.id,
+        //         target: target.id,
+        //     });
+        // });
+        // console.log(
+        //     `${amount} moved from ${source.username} to ${target.username}`
+        // );
     }
 
     async totalMoney(server: discord.Guild) {
         const row = await this.db!.get(
-            "SELECT SUM(balance) total " + "FROM users " + "WHERE server = ?",
+            "SELECT SUM(balance) total FROM users WHERE server = ?",
             server.id
         );
         if (row && row.total) {
@@ -162,9 +155,7 @@ class SqliteDataSource {
 
     async getLastSignon(user: discord.User, server: discord.Guild) {
         const row = await this.db!.get(
-            "SELECT lastSignon " +
-                "FROM users " +
-                "WHERE id = ? AND server = ?",
+            "SELECT lastSignon FROM users WHERE id = ? AND server = ?",
             user.id,
             server.id
         );
@@ -175,20 +166,17 @@ class SqliteDataSource {
     }
 
     async setLastSignon(user: discord.User, server: discord.Guild) {
-        return this.db!.run(
-            "UPDATE users " +
-                "SET lastSignon = CAST(STRFTIME('%s', 'now') AS INTEGER) " +
-                "WHERE id = ? AND server = ?",
+        await this.db!.run(
+            "UPDATE users SET lastSignon = CAST(STRFTIME('%s', 'now') AS INTEGER) WHERE id = ? AND server = ?",
             user.id,
             server.id
-        ).then(() => console.log(`${user.username}'s signon time updated`));
+        );
+        console.log(`${user.username}'s signon time updated`);
     }
 
     async getLastInPrison(user: discord.User, server: discord.Guild) {
         const row = await this.db!.get(
-            "SELECT lastStretch " +
-                "FROM users " +
-                "WHERE id = ? AND server = ?",
+            "SELECT lastStretch FROM users WHERE id = ? AND server = ?",
             user.id,
             server.id
         );
@@ -199,18 +187,17 @@ class SqliteDataSource {
     }
 
     async setLastInPrison(user: discord.User, server: discord.Guild) {
-        return this.db!.run(
-            "UPDATE users " +
-                "SET lastStretch = CAST(STRFTIME('%s', 'now') AS INTEGER) " +
-                "WHERE id = ? AND server = ?",
+        await this.db!.run(
+            "UPDATE users SET lastStretch = CAST(STRFTIME('%s', 'now') AS INTEGER) WHERE id = ? AND server = ?",
             user.id,
             server.id
-        ).then(() => console.log(`${user.username}'s prison time updated`));
+        );
+        console.log(`${user.username}'s prison time updated`);
     }
 
     async getPlayers(server: discord.Guild) {
-        return this.db!.all(
-            "SELECT id, balance " + "FROM users " + "WHERE server = ?",
+        return await this.db!.all(
+            "SELECT id, balance FROM users WHERE server = ?",
             server.id
         );
     }
